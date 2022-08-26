442ab2685aa693ac2064b54525fcbd612d599e6c
bajra <bajra.idabagus@karmagroup.com>
2022-08-26 12:04:30 +0800

    update classname style

--- assets/js/main-v2.js
+++
@@ -1,7 +0,0 @@
-const cbox = document.querySelector('#checkbox-toggle');
-const menuSection = document.getElementById('menu-section');
-
-cbox.addEventListener('click', function (e) {
-  menuSection.classList.toggle('active');
-  document.body.classList.toggle('active');
-});
