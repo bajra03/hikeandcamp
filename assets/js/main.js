af607ae37319c2041e645cc4b7e988138d08c2f3
bajra <bajra.idabagus@karmagroup.com>
2022-08-26 16:03:46 +0800

    update offer section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,1 @@
-const cbox = document.querySelector('#checkbox-toggle');
-const menuSection = document.getElementById('menu-section');
-
-cbox.addEventListener('click', function (e) {
-  menuSection.classList.toggle('active');
-  document.body.classList.toggle('active');
-});
+console.log("Wohoo")
